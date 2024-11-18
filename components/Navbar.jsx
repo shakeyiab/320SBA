2b620011f5bcf2bbc7e86161a4edddf182922fc8
shakeyiab <shakeyiabartell@gmail.com>
2024-11-18 02:52:32 -0500

    create navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav style={{ background: "#282c34", padding: "10px", color: "white" }}>
+      <h1 style={{ margin: "0" }}>Select A City</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
